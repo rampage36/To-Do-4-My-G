6d6cee1aeefe7a6d30328988131895f6eadad9ec
rampage36 <deliro.panov@icloud.com>
2023-03-19 23:48:01 +0300

    creating mui base

--- to-do-4-my-g/src/App.tsx
+++ to-do-4-my-g/src/App.tsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import './App.css';
 import { TaskList } from './components/tasklist/tasklist';
 import {v1} from 'uuid'
+import { Grid, Button } from '@mui/material';
 
 export type filterValuetype = "all" | "completed" | "active" 
 
@@ -36,7 +37,6 @@ function App() {
       )
     }
 
-
     let tasksForShow = tasks;
     if (filter === "completed") {
       tasksForShow = tasks.filter(t => t.isDone === true);
@@ -46,19 +46,48 @@ function App() {
     }
 
   return (
-   <div className='appBody'>
-      <TaskList tasks={tasksForShow}
-                removeTask={removeTask}
-                addNote={addNote}
-                closeTask={closeTask}/>
-    <div>
-      <button onClick={ () => {changeFilter("all")}}>Все задачи</button>
-      <button onClick={ () => {changeFilter("active")}}>Активно</button>
-      <button onClick={ () => {changeFilter("completed")}}>Выполненно</button>
-    </div>
-   </div>
-  
+  <div className='all'>
+        <Grid container spacing={1}>
+            
+        <Grid item xs={3}>
+            <div className='Sample1'>
+            </div>
+        </Grid>
 
+        <Grid item xs={6}>
+            <div className='appBody'>
+                <TaskList tasks={tasksForShow}
+                          removeTask={removeTask}
+                          addNote={addNote}
+                          closeTask={closeTask}/>
+                
+                <div className='btn-group'>
+                  <Button   
+                    sx={{margin: '15px'}}
+                    variant="outlined" 
+                    onClick={ () => {changeFilter("all")}}>Все задачи
+                  </Button>
+                  <Button
+                    sx={{margin: '15px'}}
+                    variant="outlined" 
+                    onClick={ () => {changeFilter("active")}}>Активно
+                  </Button>
+                  <Button
+                    sx={{margin: '15px'}}
+                    variant="outlined" 
+                    onClick={ () => {changeFilter("completed")}}>Выполненно
+                  </Button>
+                </div>
+            </div>
+        </Grid>
+
+        <Grid item xs={3}>
+              <div className='Sample2'>        
+              </div>
+        </Grid>
+
+      </Grid>
+  </div>
   );
 }
 
